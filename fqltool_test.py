3011596e80173449a0cffbd4d99e0ea8ae5f5494
Marcus Eriksson <marcuse@apache.org>
2018-10-16 16:05:25 +0200

    review

--- fqltool_test.py
+++ fqltool_test.py
@@ -5,7 +5,6 @@ import subprocess
 import tempfile
 
 from dtest import Tester
-from tools.data import rows_to_list
 from shutil import rmtree
 
 since = pytest.mark.since
@@ -14,13 +13,22 @@ logger = logging.getLogger(__name__)
 
 @since('4.0')
 class TestFQLTool(Tester):
+    """
+    Makes sure fqltool replay and fqltool compare work
+    @jira_ticket CASSANDRA-14690
+    """
     def test_replay(self):
+        """
+        Generates a full query log, wipes the nodes and replays the
+        query log, then makes sure that the data is correct.
+        @jira_ticket CASSANDRA-14690
+        """
         self.cluster.populate(2).start(wait_for_binary_proto=True)
         node1, node2 = self.cluster.nodelist()
         tmpdir = tempfile.mkdtemp()
         tmpdir2 = tempfile.mkdtemp()
-        node1.nodetool("enablefullquerylog --path=%s"%tmpdir)
-        node2.nodetool("enablefullquerylog --path=%s"%tmpdir2)
+        node1.nodetool("enablefullquerylog --path={}".format(tmpdir))
+        node2.nodetool("enablefullquerylog --path={}".format(tmpdir2))
         node1.stress(['write', 'n=1000'])
         node1.flush()
         node2.flush()
@@ -41,7 +49,7 @@ class TestFQLTool(Tester):
         got_exception = False
         try:
             node1.stress(['read', 'n=1000'])
-        except:
+        except Exception:
             got_exception = True
         assert got_exception
         # replay the log files
@@ -50,13 +58,18 @@ class TestFQLTool(Tester):
         node1.stress(['read', 'n=1000'])
 
     def test_compare(self):
+        """
+        uses fqltool replay to compare two runs of the same query log and makes
+        sure that the results match
+        @jira_ticket CASSANDRA-14690
+        """
         self.cluster.populate(1).start(wait_for_binary_proto=True)
         node1 = self.cluster.nodelist()[0]
         fqldir = tempfile.mkdtemp()
 
         node1.stress(['write', 'n=1000'])
         node1.flush()
-        node1.nodetool("enablefullquerylog --path=%s"%fqldir)
+        node1.nodetool("enablefullquerylog --path={}".format(fqldir))
         node1.stress(['read', 'n=1000'])
         node1.nodetool("disablefullquerylog")
 
@@ -69,18 +82,19 @@ class TestFQLTool(Tester):
         self._run_fqltool_replay(node1, [fqldir], "127.0.0.1", queries2, results2)
 
         output = self._run_fqltool_compare(node1, queries1, [results1, results2])
-        assert "MISMATCH" not in output # running the same reads against the same data
+        assert b"MISMATCH" not in output  # running the same reads against the same data
 
     def test_compare_mismatch(self):
         """
         generates two fql log files with different data (seq is different when running stress)
         then asserts that the replays of each generates a mismatch
+        @jira_ticket CASSANDRA-14690
         """
         self.cluster.populate(1).start(wait_for_binary_proto=True)
         node1 = self.cluster.nodelist()[0]
 
         fqldir1 = tempfile.mkdtemp()
-        node1.nodetool("enablefullquerylog --path=%s"%fqldir1)
+        node1.nodetool("enablefullquerylog --path={}".format(fqldir1))
         node1.stress(['write', 'n=1000'])
         node1.flush()
         node1.stress(['read', 'n=1000'])
@@ -93,10 +107,10 @@ class TestFQLTool(Tester):
         node1.start(wait_for_binary_proto=True)
 
         fqldir2 = tempfile.mkdtemp()
-        node1.nodetool("enablefullquerylog --path=%s"%fqldir2)
-        node1.stress(['write', 'n=1000', '-pop','seq=1000..2000'])
+        node1.nodetool("enablefullquerylog --path={}".format(fqldir2))
+        node1.stress(['write', 'n=1000', '-pop', 'seq=1000..2000'])
         node1.flush()
-        node1.stress(['read', 'n=1000','-pop','seq=1000..2000'])
+        node1.stress(['read', 'n=1000', '-pop', 'seq=1000..2000'])
         node1.nodetool("disablefullquerylog")
         node1.stop()
         for d in node1.data_directories():
@@ -117,25 +131,25 @@ class TestFQLTool(Tester):
         self._run_fqltool_replay(node1, [fqldir2], "127.0.0.1", queries2, results2)
 
         output = self._run_fqltool_compare(node1, queries1, [results1, results2])
-        assert "MISMATCH" in output # running the same reads against the same data
+        assert b"MISMATCH" in output  # compares two different stress runs, should mismatch
 
     def _run_fqltool_replay(self, node, logdirs, target, queries, results):
         fqltool = self.fqltool(node)
-        args = [fqltool, "replay", "--target %s"%target]
+        args = [fqltool, "replay", "--target {}".format(target)]
         if queries is not None:
-            args.append("--store-queries %s"%queries)
+            args.append("--store-queries {}".format(queries))
         if results is not None:
-            args.append("--results %s"%results)
+            args.append("--results {}".format(results))
         args.extend(logdirs)
         rc = subprocess.call(args)
         assert rc == 0
 
     def _run_fqltool_compare(self, node, queries, results):
         fqltool = self.fqltool(node)
-        args = [fqltool, "compare", "--queries %s"%queries]
+        args = [fqltool, "compare", "--queries {}".format(queries)]
         args.extend([os.path.join(r, "127.0.0.1") for r in results])
         logger.info(args)
-        p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
+        p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         (stdout, stderr) = p.communicate()
         logger.info(stdout)
         return stdout
