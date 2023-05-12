1199569388c91bd616196f0d4668a6e76c1b2d5d
Marcus Eriksson <marcus_eriksson@apple.com>
2023-05-12 10:54:07 +0200

    [CEP-21] PaxosUncommittedIndex now gets added to the IndexInfo table

--- secondary_indexes_test.py
+++ secondary_indexes_test.py
@@ -321,9 +321,12 @@ class TestSecondaryIndexes(Tester):
         after_files = self._index_sstables_files(node1, 'keyspace1', 'standard1', 'ix_c0')
         assert before_files != after_files
         assert 1 == len(list(session.execute(stmt, [lookup_value])))
+        expected = 1
+        if self.cluster.version() > '4.99.99':
+            expected = 2 # in tcm PaxosUncommittedIndex is is IndexInfo table
 
         # verify that only the expected row is present in the build indexes table
-        assert 1 == len(list(session.execute("""SELECT * FROM system."IndexInfo";""")))
+        assert expected == len(list(session.execute("""SELECT * FROM system."IndexInfo";""")))
 
     @since('4.0')
     def test_failing_manual_rebuild_index(self):
